ab752497fa7c79eeb1fc73cb328fce12d2f5d47f
Andreas Weber <octave@josoansi.de>
2024-09-14 15:10:49 +0200

    tests: Add docker testcases for Debian, Fedora and Ubuntu

---
+++ tests/build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -e
+
+cp ../image-acquisition-*.tar.gz .
+
+## Debian bullseye
+docker build -f Dockerfile_debian_bullseye -t debian_bullseye .
+
+## Debian bookworm
+docker build -f Dockerfile_debian_bookworm -t debian_bookworm .
+
+## Fedora 31
+docker build -f Dockerfile_fedora31 -t fedora31 .
+
+## Fedora 33
+docker build -f Dockerfile_fedora33 -t fedora33 .
+
+## Fedora 38
+docker build -f Dockerfile_fedora38 -t fedora38 .
+
+## Ubuntu 20.04, EOL Apr 2025
+## The installed kernel is video-node-centric
+## See https://www.kernel.org/doc/html/v6.1/userspace-api/media/v4l/open.html
+## which describes the media controller API for newer kernels
+docker build -f Dockerfile_ubuntu_20_04 -t ubuntu_20_04 .
+
+## Ubuntu 22.04
+docker build -f Dockerfile_ubuntu_22_04 -t ubuntu_22_04 .
+
+## Ubuntu 23.04
+docker build -f Dockerfile_ubuntu_23_04 -t ubuntu_23_04 .
+
+## Ubuntu 24.04
+docker build -f Dockerfile_ubuntu_24_04 -t ubuntu_24_04 .
+
+rm -f image-acquisition-*.tar.gz
---
+++ tests/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+OCMD='pkg load image-acquisition; system ("head -n2 /etc/os-release"); printf ("OCTAVE_VERSION = %s\n", OCTAVE_VERSION); test @videoinput/videoinput;test @videoinput/get;test @videoinput/set;test imaqhwinfo;'
+echo "octave command: ${OCMD}"
+echo
+
+do_run () {
+  # Info: If you want to play in an interactive shell, just add --persist before --eval
+  docker run --device /dev/video0 -it "$1" octave -q --eval "$OCMD"
+  echo "---"
+}
+
+do_run debian_bullseye
+do_run debian_bookworm
+do_run fedora31
+do_run fedora33
+do_run fedora38
+do_run ubuntu_20_04
+do_run ubuntu_22_04
+do_run ubuntu_23_04
+do_run ubuntu_24_04
