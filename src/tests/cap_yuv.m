5eadbfc4b71d471e3f34212a888bd3c0e510b8b5
Andreas Weber <andy@josoansi.de>
2025-01-11 12:21:54 +0100

    add lowlevel YUV capture tests

---
+++ src/tests/cap_yuv.m
@@ -0,0 +1,39 @@
+# Capture image in various YUV formats (not RGB, not MJPEG)
+
+clear all
+addpath ("../../inst") # for __test__device__
+load_imaq
+
+x = __imaq_handler_open__(__test__device__{:})
+
+if (isunix ())
+  fmts =  {"YUYV", "YU12", "YV12"};
+elseif (ispc ())
+  fmts =  {"YUY2", "NV12"};
+endif
+
+for j = 1:numel (fmts)
+
+  __imaq_handler_s_fmt__ (x, fmts{j}, [1280 720]);
+  __imaq_handler_streamon__ (x, 2)
+  n = 20;
+  tic ();
+  for k = 1:n
+    [img, seq, timestamp] = __imaq_handler_capture__(x, 0, 1);
+  endfor
+  t = toc ();
+  __imaq_handler_streamoff__ (x)
+  printf ("INFO: captured %i frames in %.2fs (%.1f fps)\n", n, t, n/t);
+
+  print_stats = @(X) printf ("%s, min: %i, mean: %i, max: %i\n", typeinfo(X), min(X(:)), mean(X(:)), max(X(:)));
+  print_stats (img.Y)
+  print_stats (img.Cb)
+  print_stats (img.Cr)
+
+  r = __imaq_handler_YCbCr_to_RGB__ (img, 601);
+  figure (j)
+  imshow(r)
+  title (fmts{j})
+
+endfor
+
