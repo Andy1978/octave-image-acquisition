bb53946cbbb0717a94c01510b0095223b511de9e
Andreas Weber <andy@josoansi.de>
2025-03-15 07:35:17 +0100

    Update Copyright comment

--- inst/@videoinput/videoinput.m
+++ inst/@videoinput/videoinput.m
@@ -1,4 +1,4 @@
-## Copyright (C) 2024 Andreas Weber <andy.weber.aw@gmail.com>
+## Copyright (C) 2024-2025 Andreas Weber <andy.weber.aw@gmail.com>
 ##
 ## This program is free software; you can redistribute it and/or modify it under
 ## the terms of the GNU General Public License as published by the Free Software
